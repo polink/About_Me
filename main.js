c99a8f57b85ed9c12d598f0631a5a86a96a96b95
Karl Polintan <kf.polintan@gmail.com>
2018-09-15 18:06:54 -0700

    Functioned Q6 and Q7; modified CSS more; added Top 10 Travel; made sure #correct works.

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var uName = function () {
   userName = prompt('Welcome to my site, what is your name?');
 
   console.log(userName);
-}
+};
+
 uName();
 //
 var rocks = function () {
@@ -23,6 +24,7 @@ var rocks = function () {
   }
 }
 rocks();
+
 //
 var kats = function () {
 
@@ -38,10 +40,10 @@ var kats = function () {
     alert(kitty + 's are cool, but Karl wants a cat.');
   }
 
-}
+};
 kats();
-//
 
+//
 var univ = function () {
 
   var skoo = prompt('What University did Karl go to?'); //Question THREE
@@ -49,13 +51,14 @@ var univ = function () {
 
   if (skoo.toLowerCase() === 'uw' || skoo.toLowerCase() === 'university of washington') {
     alert('You\'re right, Karl is a proud Husky!');
+    numCorrect++;
   } else if (skoo.toLowerCase() === 'wsu' || skoo.toLowerCase() === 'washington state university') {
     alert('That\'s very funny. Karl went to UW.');
   } else {
     alert('That is not the school that I went to.');
   }
 
-}
+};
 univ();
 
 //
@@ -67,9 +70,11 @@ var homeTownFunc = function () {
 
     case 'los angeles' || 'la':
       alert('That is the correct city!');
+      numCorrect++;
       break;
     case 'la':
       alert('That is the correct city!');
+      numCorrect++;
       break;
     case 'seattle':
       alert('I\'ve lived here for almost 25 years, but I wasn\'t born here!');
@@ -81,17 +86,27 @@ var homeTownFunc = function () {
       alert('Now you\'re just guessing.');
       break;
     default:
-      alert('Please enter something.');
+      alert('That city is unfortunately incorrect.');
       break;
   }
-}
+};
 homeTownFunc();
-var color = prompt('What is Karl\'s favorite color?'); //Question FIVE
-console.log(color);
 
-while (color.toLowerCase() !== 'orange') {
-  color = prompt('Please try again!');
-}
+//Question FIVE
+var clr = function () {
+
+  do {
+    var color = prompt('What is Karl\'s favorite color?');
+    console.log(color);
+  } while (color.toLowerCase() !== 'orange');
+
+  if (color !== 'orange') {
+    color = prompt('Please try again!');
+  } else {
+    numCorrect++;
+  }
+};
+clr();
 
 //Lab 03 Additions
 
@@ -99,61 +114,67 @@ while (color.toLowerCase() !== 'orange') {
 //Sounds like a FOR loop to be able to give the user up to 4 guesses.
 //For guessing to high/low a number, it'd make sense to use an ELSE/IF, but I know there's a more elegant method.
 //BONUS: Make the question generate a random number to guess instead.
-var dayAns = 24;
-for (var i = 4; i > 0; i--) {
-  var iTxt = i.toString();
-  var day = prompt('Now guess which day of the month I was born on! You have' + iTxt+' guesses.');
-  var dayNum = parseInt(day);
-  
-  if (dayNum === dayAns) {
-    alert('Hey thats the right answer.');
-    break;
-  } else if (dayNum > dayAns) {
-    alert('That numhber is too high.');
-  } else if (dayNum < dayAns) {
-    alert('Please enter a higher number.');
-  } else {
-    alert('Please enter a number!');
-  }
-}
-if (dayNum !== dayAns) {
-  alert('placeholder');
-}
-console.log(day);
+var bDay = function(){
+  var dayAns = 24;
+  for (var i = 4; i > 0; i--) {
+    var iTxt = i.toString();
+    var day = prompt('Now guess which day of the month I was born on! You have ' + iTxt + ' guesses.');
+    var dayNum = parseInt(day);
 
-//for ()
+    if (dayNum === dayAns) {
+      alert('Hey, that\'s the right answer.');
+      numCorrect++;
+      break;
+    } else if (dayNum > dayAns) {
+      alert('That number is too high.');
+    } else if (dayNum < dayAns) {
+      alert('Please enter a higher number.');
+    } else {
+      alert('Please enter a number!');
+    }
+  }
+  if (dayNum !== dayAns) {
+    alert('placeholder');
+  }
+  console.log(day);
+}
+bDay();
 
 //Question SEVEN - has multiple possible correct answers stored in an array.
 //Six tries to get a correct answer - so another FOR loop?
-//rick driving
-var favGames = ['overwatch', 'minecraft', 'fortnite', 'titanfall', 'mario kart', 'mega man'];
-var ansCorrect = false;
-var guessCount = 6;
-do {
+//Rick driving
+var allTheGames = function(){
+  var favGames = ['overwatch', 'minecraft', 'fortnite', 'titanfall', 'mario kart', 'mega man'];
+  var ansCorrect = false;
+  var guessCount = 6;
+  do {
 
-  var gamesAnswer = prompt('Guess one of my favorite games');
-  for (var i = 0; i < favGames.length; i++) {
-    if (favGames[i] === gamesAnswer) {
-      ansCorrect = true;
+    var gamesAnswer = prompt('Guess one of my favorite games:');
+    for (var i = 0; i < favGames.length; i++) {
+      if (favGames[i] === gamesAnswer) {
+        ansCorrect = true;
+      
+      }
     }
+    if (ansCorrect) {
+      alert('Correct! Overwatch, Minecraft, Fortnite, Titanfall, Mario Kart, and Mega Man are my favorite games.');
+      numCorrect++;
+    } else {
+      alert('Nice try, guess again.');
+    }
+    guessCount--;
+
+  } while (guessCount > 0 && !ansCorrect);
+  if (!ansCorrect) {
+    alert('Too bad. Overwatch, Minecraft, Fortnite, Titanfall, Mario Kart, and Mega Man are my favorite games.');
   }
-  if (ansCorrect) {
-    alert('Correct! Overwatch, Minecraft, Fortnite, Titanfall, Mario Kart, and Mega Man are my favorite games.');
-  } else {
-    alert('Nice try guess again.');
-  }
-  guessCount--;
 
-} while (guessCount > 0 && !ansCorrect);
-if (!ansCorrect) {
-  alert('Too bad. Overwatch, Minecraft, Fortnite, Titanfall, Mario Kart, and Mega Man are my favorite games.');
-}
-
-console.log(gamesAnswer);
-
+  console.log(gamesAnswer);
+};
+allTheGames();
 //Final - add up the number of correct answers and display to the user.
 
 //BONUS: make another HTML file with the questions, and fill the answers in after the user provides answers.
-alert('You got '+numCorrect+' out of 7.');
+alert('You got ' + numCorrect + ' out of 7.');
 console.log(numCorrect);
 //BONUS: make another HTML file with the questions, and fill the answers in after the user provides answers.
